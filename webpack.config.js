9ca8c2c881e7df0d58e06b2c8f78e396f818fc3d
furus <furusu0715@gmail.com>
2018-10-13 22:22:14 +0900

    開発サーバーの立ち上げ webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,8 @@
 const path = require('path');
+//webpackモジュールを読み込む
+const webpack = require('webpack');
+//html-webpack-pluginモジュールを読み込む
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const src = path.join(__dirname, 'src');
 const dist = path.join(__dirname, 'dist');
@@ -16,15 +20,15 @@ module.exports = {
   },
   resolve: {
     //import文のパス指定にnode_modulesを省略できるようにします
-    modules:['node_modules'],
+    modules: ['node_modules'],
     //.jsまたは.jsxの拡張子を省略できるようにします
-    extensions:['.js','.jsx']
+    extensions: ['.js', '.jsx']
   },
   module: {
     rules: [
       {
         //ルールを適用するファイルの正規表現
-        test:/\.(js|jsx)$/,
+        test: /\.(js|jsx)$/,
         //node_modules以下のファイルには適用しないようにします
         exclude: /node_modules/,
         //使用する loader
@@ -32,6 +36,17 @@ module.exports = {
       }
     ]
   },
+  devServer: {
+    contentBase: dist, //開発サーバーを立ち上げる参照ディレクトリ
+    hot: true, //hot-reloadを有効にます
+    port: 3000 //サーバーを立ち上げるポート番号
+  },
+  plugins: [
+    //hot-reloadを有効にするプラグインを追加
+    new webpack.HotModuleReplacementPlugin(),
+    //HtmlWebpackPluginを追加
+    new HtmlWebpackPlugin()
+  ],
   //sourceMappingの設定
   devtool: 'cheap-module-eval-source-map'
 };
