654a525a6ae9325a03a914f5d51090a49796eaee
kf07 <furusu0715@gmail.com>
2018-10-16 15:58:16 +0900

    jest導入 test/index.test.jsで src/js/index.jsのテストコードを書いてみる

---
+++ test/index.test.js
@@ -0,0 +1,30 @@
+import { Hello } from '../src/js/index';
+
+const name ='jest';
+let hello;
+
+describe('Hello Class Test',()=>{
+  beforeEach(()=>{
+    hello = new Hello(name);
+  });
+
+  test('We can check if the name defined the class constructor',()=> {
+    expect(hello.name).toBe(name);
+  });
+
+  //say()メソッドのテスト
+  test('We can check if console.log() called on say() method',()=>{
+    const spy = jest.spyOn(console,'log');
+
+    hello.say();
+
+    //spyしたメソッドが呼ばれていることを確認する
+    expect(spy).toHaveBeenCalledWith(`Hello ${name} World!`);
+
+    spy.mockReset();
+    spy.mockRestore();
+  })
+});
+
+
+
